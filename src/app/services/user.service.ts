8fd128442116a27e46b055fe6c8bf51a7d0021af
Thorbjørn Damkjær <thsd@cloudfactory.dk>
2021-09-06 12:23:08 +0200

    combined http and socket fromevent

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,40 +1,52 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Socket } from 'ngx-socket-io';
-import { map, tap } from 'rxjs/operators';
 import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
-  //TODO Actual endpoint route here
   private readonly BASE_URL: string = 'http://localhost:5000/users';
   private countBehavior: BehaviorSubject<number> = new BehaviorSubject(0);
-  private usersBehavior: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+  private usersBehavior: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(
+    []
+  );
   private count$: Observable<number> = this.countBehavior.asObservable();
   private users$: Observable<User[]> = this.usersBehavior.asObservable();
-  constructor(private client: HttpClient, private socket: Socket) {
-    this.socket.fromEvent<number>('active-users-count').subscribe((count) => {
-      this.countBehavior.next(count);
-    })
-    this.socket.fromEvent<User[]>('active-users').subscribe((users) => {
-      console.log(users);
-      this.usersBehavior.next(users);
-    });
-  }
+  constructor(private client: HttpClient, private socket: Socket) {}
 
   enterChatroom(userName: string): Observable<any> {
     return this.client.post(this.BASE_URL, { username: userName });
   }
 
-  getLiveUsersCount(): Observable<number> {
+  public getUsersCount(): Observable<number> {
+    this.client.get<number>(this.BASE_URL + '/count').subscribe((count) => {
+      this.countBehavior.next(count);
+    });
+    this.socket.fromEvent<number>('active-users-count').subscribe((count) => {
+      this.countBehavior.next(count);
+    });
     return this.count$;
   }
 
-  getLiveUsers(): Observable<User[]> {
+  public getUsers(): Observable<User[]> {
+    this.client.get<User[]>(this.BASE_URL).subscribe((users) => {
+      this.usersBehavior.next(users);
+    });
+    this.socket.fromEvent<User>('active-users-new').subscribe((user) => {
+      const users = this.usersBehavior.value;
+      this.usersBehavior.next([...users, user]);
+    });
     return this.users$;
+  }
+
+  getLiveUsersCount(): Observable<number> {
+    return this.getUsersCount();
+  }
+
+  getLiveUsers(): Observable<User[]> {
+    return this.getUsers();
   }
 }
